49576f6ee825eec20fe90de187a44e370e97a367
edmond-khoo-rp <125122558+edmond-khoo-rp@users.noreply.github.com>
2025-05-25 15:16:12 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DataDefenseGame from "./DataDefenseGame";
+
+const App = () => {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>🎴 Data Defense Card Game</h1>
+      <DataDefenseGame />
+    </div>
+  );
+};
+
+export default App;
